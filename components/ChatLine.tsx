be85d8ddf1cf1a6130b1cac8ed9ca89afad75f3c
Farhat, Ayad (Data61, Eveleigh) <Ayad.Farhat@data61.csiro.au>
2023-01-09 15:38:08 +1100

    added copy to clipboard

--- components/ChatLine.tsx
+++ components/ChatLine.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Badge, Card, Container, Spacer, Text } from '@nextui-org/react'
+import { Avatar, Badge, Card, Container, Row, Spacer, Text } from '@nextui-org/react'
 import clsx from 'clsx'
 import Balancer from 'react-wrap-balancer'
 
@@ -43,10 +43,14 @@ export function ChatLine({ who = 'bot', message }: Message) {
             <Spacer y={0.5} x={0} />
             <Card isHoverable isPressable css={who == 'bot' ? {
                 mw: "350px"
-            } : { mw: "350px", float: 'right', position: "relative" }} variant="bordered">
-
+            } : { mw: "350px", float: 'right', position: "relative" }} variant="bordered" onPress={() => { navigator.clipboard.writeText(message) }}
+            >
                 <Card.Body >
-                    <Text >{message}</Text>
+                    <Row>
+
+                        <Text >{message}</Text>
+                        {who == 'bot' && <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" /></svg>}
+                    </Row>
                 </Card.Body>
             </Card>
             {/* </Balancer > */}
